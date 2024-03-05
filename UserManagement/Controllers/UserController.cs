c58a1cf68630f3e608a01631a4f8a9d912349fa6
Somayazaid2 <somayazaid88@gmail.com>
2024-03-05 21:24:21 +0200

    Assign Role to users

---
+++ UserManagement/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Models;
+using UserManagement.ViewModels;
+
+namespace UserManagement.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserController(UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager)
+        {
+            _userManager  = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.Select(user => new UserViewModel {
+                Id = user.Id ,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                UserName = user.UserName,
+                Roles =  _userManager.GetRolesAsync(user).Result
+            }).ToList();
+            return View(users);
+        }
+
+        public async Task<IActionResult> ManageRoles(string userId)
+        {
+            var user  = await _userManager.FindByIdAsync(userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            var viewModel = new UserRoleViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = roles.Select(roles => new RoleViewModel { RoleId = roles.Id, RoleName = roles.Name, IsSelected = _userManager.IsInRoleAsync(user, roles.Name).Result }).ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
